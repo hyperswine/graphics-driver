748724c05efe4f9e9ab5d69229cb68bc4605e9ef
Jason Qin <jiasheng.qin@student.unsw.edu.au>
2022-08-03 18:11:08 +1000

    Add RT skel 1

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,14 @@
 // wraps around multiple GPUs. Handles requests for multiple gpus
 
 pub struct DirectRenderManager {}
+
+// -----------------
+// Spectral RayTrace
+// -----------------
+
+// 1. convert a RayTrace scene description (.yml) through compiler into .rt binary
+// 2. convert a cpu game program into ELF. When the functions (these functions below) are called, copy command buffer and tell RTU to ray trace
+
+fn cast_ray() {}
+
+// Prob better on userland!!
